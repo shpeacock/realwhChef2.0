071c8c1d2a3c84d89eb2904f70d4bcc65e8d3c08
shpeacock <spencerpeacock@gmail.com>
2019-11-06 21:20:20 -0600

    IDK making it better????

--- index.js
+++ index.js
@@ -7,10 +7,9 @@ const bf = m.breakfast;
 //this is creating a twit object using API tokens established in a file that is ignored from git
 const chefConfig = require('./chef.js');
 const whChef = new twit(chefConfig);
-const loudSoundConfig = require('./henry.js');
-const loudSound = new twit(loudSoundConfig);
 const cron = require('node-cron');
 const cronTime = require('cron-time-generator');
+const cronJob = require('cron').CronJob;
 const myid_str = [925160703006330880, 22831053, 1096867112029691911,];
 
 //this function creates a random breakfast purportedly served to the president of the united states and tweets it
@@ -24,25 +23,12 @@ function breakFast(){
 }
 
 
-var stream = whChef.stream('statuses/filter', { track: '@realwhChef' });
-  stream.on('tweet', function(tweet){
-        tweetId = tweet.user.id_str;
-        if(myid_str.includes(tweetId) ){
-          let statusObj = {
-            status: 'yes master',
-            in_reply_to_status_id: tweet.id_str
-          }
-          console.log('one of my bois just tweeted'+ tweet.user.id_str);
-          // whChef.post('statuses/update', statusObj);
-        }
-        else {
-          let statusObj = {status: "sup @" + tweet.user.screen_name + "? did you really just tweet at a bot? That's so adorable!!! This is an automated response. I will likely never read your tweet/mention. Weeelp. No news is good news right?? I'm assuming you're singing my praises. Have a great day fam!",
-          in_reply_to_status_id: tweet.id_str
-          }
-          // whChef.post('statuses/update', statusObj);
-          console.log('ohhh shit stranger danger!' + tweet.user.id_str);
-        }
-});
+// var stream = whChef.stream('statuses/filter', { track: '@realwhChef' });
+//   stream.on('tweet', function(tweet){
+//     tweetId = tweet.user.id_str;
+//     person = tweet.user.screen_name;
+//     whChef.post('statuses/update', {status: person + ' What gewd fam?! I appreciate the good vibes', in_reply_to_status_id: tweetId});
+// });
 
 function searchIt() {
 whChef.get('search/tweets', {q: "\" Trump Dick\"",}  , gotData);
@@ -65,13 +51,24 @@ whChef.get('search/tweets', {q: "\" Trump Dick\"",}  , gotData);
   }
 }
 
-cron.schedule(cronTime.everyDayAt(6), function () {
-  breakFast();
+//this makes sure that the breakfast function runs everymorning at 8:00am EST
+var breakFastJob = new cronJob({
+  cronTime: '45 5 * * *',
+  onTick: function(){
+    breakFast();
+  },
+  start: false,
+  timeZone: 'America/Chicago'
+});
+//this looks for followers every sunday, wednesday and friday at 715 PM CST
+var findFolks = new cronJob({
+  cronTime: '0 22 * * *',
+  onTick: function(){
+    searchIt();
+  },
+  start: false,
+  timeZone: 'America/Chicago'
 });
 
-//this looks for followers every sing day at 1010
-cron.schedule(cronTime.everyDayAt(10, 10), function() {
-  searchIt();
-});
-
-
+findFolks.start();
+breakFastJob.start();
